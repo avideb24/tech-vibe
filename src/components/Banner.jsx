05e6381fcb0415d98eaab1fe7dbd6da551776370
avideb24 <debavi24@gmail.com>
2023-10-06 21:06:42 +0600

    added banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,16 @@
+import TechBg from '../assets/tech-bg.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            baner
+        <div className='relative'>
+            <img src={TechBg} className='object-cover' alt="banner image" />
+            <div className='absolute w-full h-full left-0 top-0 flex justify-end items-center px-5'>
+                <div className='max-w-2xl text-right space-y-6'>
+                    <h3 className='text-lg font-bold'>Biggest Tech Event</h3>
+                    <h1 className='text-6xl font-bold pr-6 border-r-8 border-r-[#108aee]'>Growing The Global Technology Industry</h1>
+                    <p className='pl-24'>Dive And Experience Hands-on Workshops, Inspiring Talks, and the Future of Tech, All Under One Roof! Connect with Industry Leaders, Engage in Cutting-Edge Demos, and Shape the Future Together. Embrace the Tech Revolution – Your Gateway to Tomorrow Awaits!</p>
+                </div>
+            </div>
         </div>
     );
 };
