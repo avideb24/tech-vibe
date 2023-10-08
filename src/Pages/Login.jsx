854e69844acad83242148e564e1534d100a6c096
avideb24 <debavi24@gmail.com>
2023-10-08 21:10:16 +0600

    change input bg

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -69,8 +69,8 @@ const Login = () => {
                 <div className="max-w-xl mx-auto bg-[#132151] p-10 rounded-md text-white">
                     <h2 className="text-center sm:text-3xl text-2xl font-bold py-9 border-b-2 text-yellow-500 border-b-yellow-500">Login your account</h2>
                     <form className="mt-10 text-blue-950" onSubmit={handleRegister}>
-                        <input className="w-full border-2 border-white p-2 rounded-md" type="email" name="email" required placeholder="Enter your email address" />
-                        <input className="w-full border-2 border-white p-2 my-4 rounded-md" type="password" name="password" required placeholder="Enter your password" />
+                        <input className="w-full border-2 bg-white border-white p-2 rounded-md" type="email" name="email" required placeholder="Enter your email address" />
+                        <input className="w-full border-2 bg-white border-white p-2 my-4 rounded-md" type="password" name="password" required placeholder="Enter your password" />
                         <input type="submit" className="w-full bg-yellow-500 text-[#08133a] font-bold py-2 rounded-md cursor-pointer" value="Login" />
                     </form>
                     <p className="text-center mt-6 sm:text-md text-sm">Dont’t Have An Account ? <Link className="font-bold text-blue-500" to='/register'>Register</Link></p>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -83,10 +83,10 @@ const Register = () => {
                 <div className=" max-w-xl mx-auto bg-[#132151]  p-10 rounded-md text-white">
                     <h2 className="text-center sm:text-3xl text-2xl font-bold py-9 border-b-2 text-yellow-500 border-b-yellow-500">Register your account</h2>
                     <form className="mt-10 text-blue-950" onSubmit={handleRegister}>
-                        <input onChange={handleUserName} className="w-full border-2 border-white p-2 rounded-md" type="text" name="name" required placeholder="Enter your name" />
-                        <input onChange={handleUserPhoto} className="w-full border-2 border-white p-2 rounded-md my-4" type="text" name="photo" required placeholder="Enter your photo URL" />
-                        <input className="w-full border-2 border-white p-2 rounded-md" type="email" name="email" required placeholder="Enter your email address" />
-                        <input className="w-full border-2 border-white p-2 my-4 rounded-md" type="password" name="password" required placeholder="Enter your password" />
+                        <input onChange={handleUserName} className="w-full border-2 bg-white border-white p-2 rounded-md" type="text" name="name" required placeholder="Enter your name" />
+                        <input onChange={handleUserPhoto} className="w-full border-2 bg-white border-white p-2 rounded-md my-4" type="text" name="photo" required placeholder="Enter your photo URL" />
+                        <input className="w-full border-2 border-white p-2 bg-white rounded-md" type="email" name="email" required placeholder="Enter your email address" />
+                        <input className="w-full border-2 border-white bg-white p-2 my-4 rounded-md" type="password" name="password" required placeholder="Enter your password" />
                         <input type="submit" className="w-full bg-yellow-500 text-[#08133a] font-bold py-2 rounded-md cursor-pointer" value="Register" />
                     </form>
                     <p className="text-center mt-6 sm:text-md text-sm">Already Have An Account ? <Link className="font-bold text-blue-500" to='/login'>Login</Link></p>
