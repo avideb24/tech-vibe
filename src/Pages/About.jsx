8038ff693238d8f7b31b621d373f9ad3e4bdc6b6
avideb24 <debavi24@gmail.com>
2024-02-20 17:24:05 +0600

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
                     <p className="pt-5"><span className="text-2xl font-bold">TechVibe</span> is the heartbeat of the tech community, connecting passionate individuals with the latest trends, innovations, and thought leaders in the technology realm. Our mission is to bridge the gap between tech enthusiasts and the most impactful events happening worldwide.</p>
                     <p>At TechVibe, we curate a diverse array of tech events, ranging from hackathons and startup meetups to industry conferences and product launches. We believe in the power of knowledge sharing and networking, empowering professionals and enthusiasts alike to stay ahead in this ever-evolving digital landscape.</p>
                     <p>Join us on this exhilarating journey of discovery and inspiration. Whether you&#39;re a seasoned developer, an aspiring entrepreneur, or simply curious about the future of technology, TechVibe is your compass in the dynamic world of tech events.</p>
-                    <p>Stay tuned, stay inspired, and let&#39;s ride the wave of innovation together!</p>
+                    <p>Stay tuned, stay inspired, and let&#39; ride the wave of innovation together!</p>
                 </div>
             </div>
             <Footer></Footer>
